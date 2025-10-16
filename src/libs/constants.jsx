a7d759bd2838e59f431e8c6bdd1970523c76f704
Tabish Zaman <tabish.zaman@socialpie.io>
2025-10-16 15:18:38 +0500

    completed method 4

--- src/libs/constants.jsx
+++ src/libs/constants.jsx
@@ -3,12 +3,12 @@ import EmailIcon from "../assets/images/icons/email.svg";
 export const BASE_URL = "https://directory.bmibook.com/api/api/v1/";
 export const COMPANY_URL = "https://directory.bmibook.com/company/view/";
 
-// export const SERVER_URL = "https://coupon.masking.sptest.dev/api/deals/";
-// export const PROXY_URL = "https://coupon.masking.sptest.dev/proxy/";
-// export const MRTHOD1_URL = "https://coupon.masking.sptest.dev/proxy-method1/";
-export const PROXY_URL = "http://localhost:4000/";
-export const MRTHOD1_URL = "http://localhost:4200/";
-export const SERVER_URL = "http://localhost:5000/deals";
+export const SERVER_URL = "https://coupon.masking.sptest.dev/api/deals/";
+export const PROXY_URL = "https://coupon.masking.sptest.dev/proxy/";
+export const MRTHOD1_URL = "https://coupon.masking.sptest.dev/proxy-method1/";
+// export const PROXY_URL = "http://localhost:4000/";
+// export const MRTHOD1_URL = "http://localhost:4200/";
+// export const SERVER_URL = "http://localhost:5000/deals";
 
 export const UserIcons = [
   {
